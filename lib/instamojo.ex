6c8711d1330f0ffcd12610427783a22803923737
Sasank Yadavalli <yadavallisasank@gmail.com>
2017-11-02 15:08:24 -0700

    Initial commit with basic scaffolding

---
+++ lib/instamojo.ex
@@ -0,0 +1,18 @@
+defmodule Instamojo do
+  @moduledoc """
+  Documentation for Instamojo.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Instamojo.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/instamojo_test.exs
@@ -0,0 +1,8 @@
+defmodule InstamojoTest do
+  use ExUnit.Case
+  doctest Instamojo
+
+  test "greets the world" do
+    assert Instamojo.hello() == :world
+  end
+end
